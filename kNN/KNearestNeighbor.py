5c4a76d4118760f4c893b16e492815569876568d
sudharsana-kjl <kjlsudharsana@gmail.com>
2017-09-05 07:22:47 +0530

    implement kNearestNeighbor in iris dataset

---
+++ kNN/KNearestNeighbor.py
@@ -0,0 +1,40 @@
+import math
+import operator
+
+def euclideanDistance(instance1, instance2, length):
+	distance = 0
+	for x in range(length):
+		distance += pow((instance1[x] - instance2[x]), 2)
+	return math.sqrt(distance)
+
+def getNeighbors(trainingSet, testInstance, k):
+	distances = []
+	length = len(testInstance)-1
+	for x in range(len(trainingSet)):
+		dist = euclideanDistance(testInstance, trainingSet[x], length)
+		distances.append((trainingSet[x], dist))
+	distances.sort(key=operator.itemgetter(1))
+	neighbors = []
+	for x in range(k):
+		neighbors.append(distances[x][0])
+	return neighbors
+
+def getResponse(neighbors):
+	classVotes = {}
+	for x in range(len(neighbors)):
+		response = neighbors[x][-1]
+		if response in classVotes:
+			classVotes[response] += 1
+		else:
+			classVotes[response] = 1
+	sortedVotes = sorted(classVotes.iteritems(), key=operator.itemgetter(1), reverse=True)
+	return sortedVotes[0][0]
+
+def getAccuracy(testSet, predictions):
+	correct = 0
+	for x in range(len(testSet)):
+		if testSet[x][-1] == predictions[x]:
+			correct += 1
+	return (correct/float(len(testSet))) * 100.0
+
+
--- kNN/main.py
+++ kNN/main.py
@@ -1,1 +1,22 @@
-import numpy
+import loadDataset
+import KNearestNeighbor
+	
+trainingSet=[]
+testSet=[]
+split = 0.67
+
+loadDataset.loadDataset('iris.data', split, trainingSet, testSet)
+print 'Train set: ' + repr(len(trainingSet))
+print 'Test set: ' + repr(len(testSet))
+# generate predictions
+predictions=[]
+k = 3
+for x in range(len(testSet)):
+	neighbors = KNearestNeighbor.getNeighbors(trainingSet, testSet[x], k)
+	result = KNearestNeighbor.getResponse(neighbors)
+	predictions.append(result)
+	print('> predicted=' + repr(result) + ', actual=' + repr(testSet[x][-1]))
+accuracy = KNearestNeighbor.getAccuracy(testSet, predictions)
+print('Accuracy: ' + repr(accuracy) + '%')
+	
+
